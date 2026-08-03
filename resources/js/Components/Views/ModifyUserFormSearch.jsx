336d13f9f60ff80febe78ce842c92f9e94a1939a
informaticaoal <informaticaoalht@gmail.com>
2026-08-03 09:30:45 +0200

    console.log borrado

--- resources/js/Components/Views/ModifyUserFormSearch.jsx
+++ resources/js/Components/Views/ModifyUserFormSearch.jsx
@@ -454,7 +454,6 @@ export default function ModifyUserFormSearch({
             }
             const usuariosRef = collection(db, 'usuarios');
             const dni = getValues2('dni');
-            console.log('DNI a verificar:', dni);
             const q = query(usuariosRef, where('dni', '==', dni));
             const querySnapshot = await getDocs(q);
             querySnapshot.forEach((doc) => {
